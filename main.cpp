6c6d9a74045ab8c7182f89e4dbfbfaf5781a0829
Bilal Ghani <byllghani584@gmail.com>
2026-06-21 13:35:58 +0000

    Done with the seriesGameMode function, Would be working on the endlessGameMode soon taking a break and then I'd finish up every thing plus *arrays for the moves was an easy and simple alternative*

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <random>
 
-int seriesGameMode(int range) {
+void seriesGameMode(int range) {
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	int drawCount = 0;
@@ -9,19 +9,19 @@ int seriesGameMode(int range) {
 	int loseCount = 0;
 	int playerMove;
 	for (int i = 1; i <= range; i++) {
+		std::system("cls");
 		std::uniform_int_distribution<> distr(1, 3);
 		std::cout << "==========================================\n";
 		std::cout << "         Best of " << range << " Series   \n";
 		std::cout << "==========================================\n\n";
 		std::cout << "Round " << i << " of " << range << " \n\n";
 		std::cout << "Select your move:\n";
-		std::cout << "1. Rock";
-		std::cout << "2. Paper";
-		std::cout << "3. Scissors";
+		std::cout << "1. Rock\n";
+		std::cout << "2. Paper\n";
+		std::cout << "3. Scissors\n";
 		std::cout << "Enter your choice: ";
-		std::cin >> playerMove;
 
-		while (!(std::cin >> playerMove) || (playerMove < 1 || playerMove > 5)) {
+		while (!(std::cin >> playerMove) || (playerMove < 1 || playerMove > 3)) {
 			if (std::cin.fail()) {
 				std::cin.clear();
 				std::cin.ignore(1000, '\n');
@@ -37,29 +37,86 @@ int seriesGameMode(int range) {
 
 		if (playerMove == 1) {
 			if (playerMove == computerMove) {
-				std::cout << "Your move was Rock.\n";
+				std::cout << "\nYour move was Rock.\n";
 				std::cout << "Computer Move was Rock.\n";
 				std::cout << "This round is a draw.\n";
 				drawCount++;
 			}
 			else if (computerMove == 2) {
-				std::cout << "Your move was Rock.\n";
+				std::cout << "\nYour move was Rock.\n";
 				std::cout << "Computer move was Paper.\n";
 				std::cout << "You lose this round.\n";
 				loseCount++;
-				}
+			}
 			else if (computerMove == 3) {
-				std::cout << "Your move was Rock.\n";
+				std::cout << "\nYour move was Rock.\n";
 				std::cout << "Computer move was Scissors.\n";
 				std::cout << "You win this round.\n";
+				winCount++;
 			}
 		} else if (playerMove == 2){
+			if (playerMove == computerMove) {
+				std::cout << "\nYour Move was Paper.\n";
+				std::cout << "Computer move was Paper.\n";
+				std::cout << "This round is a draw.\n";
+				drawCount++;
+			}
+			else if (computerMove == 1) {
+				std::cout << "\nYour move was Paper.\n";
+				std::cout << "Computer move was Rock.\n";
+				std::cout << "You win this round.\n";
+				winCount++;
+			}
+			else if (computerMove == 3) {
+				std::cout << "\nYour move was Paper.\n";
+				std::cout << "Computer move was Scissors.\n";
+				std::cout << "You lose this round.\n";
+				loseCount++;
+			}
 		}
+		else if (playerMove == 3) {
+			if (playerMove == computerMove) {
+				std::cout << "\nYour move was Scissors.\n";
+				std::cout << "Computer move was Scissors.\n";
+				std::cout << "This round is a draw";
+				drawCount++;
+			}
+			else if (computerMove == 1) {
+				std::cout << "\nYour move was Scissors.\n";
+				std::cout << "Computer move was Rock.\n";
+				std::cout << "You lose this round.\n";
+				loseCount++;
+			}
+			else if (computerMove == 2) {
+				std::cout << "\nYour move was Scissors.\n";
+				std::cout << "Computer move was Paper.\n";
+				std::cout << "You win this round.\n";
+				winCount++;
+			}
+		}
+		
+		std::cout << "\n Press Enter to continue to the next round";
+		std::cin.get();
 
 	}
+
+	//score check after the total rounds completed
+	std::system("cls");
+	std::cout << "\n---------------Game Over---------------\n\n";
+	std::cout << "Draw: " << drawCount << std::endl;
+	std::cout << "Lose: " << loseCount << std::endl;
+	std::cout << "Win : " << winCount << std::endl;
+	std::cout << "\n--------------------------------------------\n";
+
+	std::cout << "\nPress Enter to continue";
+	std::cin.ignore(1000, '\n');
+	std::cin.get();
 }
 
+void endlessGameMode();
+
 int main (){
+	std::system("cls");
 	int choice;
 	do {
 		std::cout << "================================================\n";
@@ -85,6 +142,15 @@ int main (){
 			}
 		}
 
+		if (choice == 1) {
+			seriesGameMode(3);
+		}
+		else if (choice == 2) {
+			seriesGameMode(5);
+		}
+		else if (choice == 3) {
+			seriesGameMode(10);
+		}
 
 
 	} while (choice != 5);
